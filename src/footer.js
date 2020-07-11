7e0ea6515fba5d5ead45cc3448572afc81c1bb15
Krocketeer <Krocketeer@github.com>
2020-07-11 11:58:12 -0700

    adapted Evan's fix to namePicker.js

--- src/footer.js
+++ src/footer.js
@@ -4,24 +4,24 @@ function Footer(props) {
     const [text, setText] = useState("")
     return  <footer className="footer">
         <input value={text}
-            onChange={e => setText(e.target.value)}
-           onKeyPress={e=> {
-               if(e.key==='Enter' && text) {
-                   props.onSend(text)
-                   setText('')
-               }
-           }}
-            placeholder="Type your message here!"
+               onChange={e => setText(e.target.value)}
+               onKeyPress={e=> {
+                   if(e.key==='Enter' && text) {
+                       props.onSend(text)
+                       setText('')
+                   }
+               }}
+               placeholder="Type your message here!"
         />
         <div id="message_bar">
             <img src="https://cdn0.iconfinder.com/data/icons/arrows-31/100/arrow-41-512.png"
-                alt="send arrow"
-                onClick={() => {
-                    if (text) {
-                        props.onSend(text)
-                        setText("")
-                    }
-                }}
+                 alt="send arrow"
+                 onClick={() => {
+                     if (text) {
+                         props.onSend(text)
+                         setText("")
+                     }
+                 }}
             />
         </div>
     </footer>
